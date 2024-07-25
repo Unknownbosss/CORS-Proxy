e2b1625d93b116eb352427ebdaeb9dcd9553da43
Unknownbosss <udeigweemmanuel0@gmail.com>
2024-07-25 21:41:48 +0100

    used app.all

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api', (req, res) => {
+app.all('*', (req, res) => {
     const url = req.query.url; // Get the target URL from the query parameter
     const apiKey = req.query.key; // Get the API key from the query parameter
     const targetUrl = `${url}?key=${apiKey}`; // Construct the full target URL with API key
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    console.log(`Target URL: ${targetUrl}`);
-
     req.pipe(require('https').createServer({ target: targetUrl }).req).pipe(res);
   });
+  
   
   
 
